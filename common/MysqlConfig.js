62ff8ac53f8c360fb8c9ed0cbd95fc9983d2aa8f
吴翰文 <p78o233@126.com>
2021-02-05 10:53:54 +0800

    数据库配置

--- common/MysqlConfig.js
+++ common/MysqlConfig.js
@@ -1,20 +1,20 @@
 // 数据库链接   cnpm install mysql --save，只会在server里面创建一次
-var mysqlConfig      =  require('mysql');
+var mysqlConfig = require('mysql');
 // https://www.cnblogs.com/shenlonghun/p/6122034.html
 var connection = mysqlConfig.createPool({
-    host : '127.0.0.1',
-    port : 3306,
-    user : 'root',
-    password : 'root',
-    database : 'oa',
-    charset:"utf8mb4",
-    timezone:"local",
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'oa',
+    charset: "utf8mb4",
+    timezone: "local",
 
 })
-connection.getConnection(function(err,conn){
-    if(err){
+connection.getConnection(function (err, conn) {
+    if (err) {
         console.log('与mysql数据库建立连接失败');
-    }else{
+    } else {
         console.log('与mysql数据库建立连接成功');
     }
 })
